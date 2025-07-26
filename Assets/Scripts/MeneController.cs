39e422d789a0e029bf9d35ef0ea1f527e5ba5928
JuanPHerm <juan.hermosillo@saei.mx>
2025-07-25 22:57:29 -0600

    Menu 2
    Ya hay funcionalidad en el boton y se espera 1 seg a cambiar de escena, falta el tween

--- Assets/Scripts/MeneController.cs
+++ Assets/Scripts/MeneController.cs
@@ -1,16 +1,24 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
 
 public class MeneController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public string sceneNameToLoad;
+    public RectTransform targetToTween;
+
+    public float animationDuration = 1.0f;
+
+    public void IniciarPartida()
     {
-        
+        targetToTween.DOSizeDelta(new Vector2(Screen.width, Screen.height),0.5f).SetEase(Ease.OutExpo).OnComplete(() =>
+        {
+            SceneManager.LoadScene("SampleScene");
+        });
     }
 
-    // Update is called once per frame
-    void Update()
+    public void CerrarJuego()
     {
-        
+
     }
 }
