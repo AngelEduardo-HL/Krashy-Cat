9a4d1c95bef1c655d2a41ea256edcf2311acf2bf
Alejandra05gsr <79392781+Alejandra05gsr@users.noreply.github.com>
2025-09-01 12:41:50 -0600

    Modelos 3D interactuable
    Modelos implementados y funcionales.

--- Assets/Scripts/CajasRompibles.cs
+++ Assets/Scripts/CajasRompibles.cs
@@ -2,50 +2,57 @@ using UnityEngine;
 
 public class CajasRompibles : MonoBehaviour
 {
-    public int health = 1;
-
     public GameObject fishPrefab;
-    public GameObject maskPrefab;
+    public GameObject shieldPrefab;
     public GameObject hpPrefab;
 
     int lootInstance;
     Vector3 placeToInstance;
 
+    public Transform playerTransform;
     public void Start()
     {
         placeToInstance = this.transform.position;
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+
     }
     public void TakeDamage(int damage)
     {
-        ////health -= damage;
-        ////if (health <= 0)
-        ////{
-        ////    Destroy(gameObject);
-        ////}
+
     }
 
    public void SpawnLoot()
     {
         //Probabilidad de que salga
         float random = Random.value;
-        if (random <= .2) lootInstance = 3;
-        else if (random >= .3 && random <=.5)  lootInstance = 2;
-        else if (random >=.6) lootInstance = 1;
-       
+        if (random <= .1) lootInstance = 0; //Loot HP
+        else if (random >= .2 && random <= .3) lootInstance = 1; //Loot shield
+        else if (random >= .4 && random <= .6) lootInstance = 2; //Loot puntos
+        else lootInstance = 3; // Sin loot
+
         //Se instancia el objeto de acuerdo al random
         switch (lootInstance)
         {
             case 0:
-                Instantiate(fishPrefab, placeToInstance, Quaternion.identity);
-                Debug.Log("Fish");
+                Instantiate(hpPrefab, placeToInstance, Quaternion.identity);
+                Debug.Log("Loot HP");
                 break;
             case 1:
-                Instantiate(maskPrefab, placeToInstance, Quaternion.identity);
-                Debug.Log("Mask");
+                if (GameObject.FindAnyObjectByType<Shield>() == null)
+                {
+                    GameObject shield = Instantiate(shieldPrefab, placeToInstance, Quaternion.identity);
+                }
+                else
+                {
+                    Debug.Log("Ya hay un Shield activo, no se instancia otro");
+                }
                 break;
             case 2:
-                Instantiate(hpPrefab, placeToInstance, Quaternion.identity);
-                Debug.Log("HP");
+                Instantiate(fishPrefab, placeToInstance, Quaternion.identity);
+                Debug.Log("Loot Points");
+                break;
+            case 3:
+                Debug.Log("No Loot");
                 break;
         }
 
